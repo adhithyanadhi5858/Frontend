2bcbb80487efcf9f11d8dc122ad237a50fe8b194
Adithyan K Chandran <adhithyanadhi5858@gmail.com>
2025-03-15 13:46:40 +0530

    error testing 1

--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -1,55 +1,109 @@
-import React, { useEffect, useState } from 'react'
-import Header from '../components/User/Header'
-import Footer from '../components/User/Footer'
-import { Outlet, useLocation } from 'react-router-dom'
-import BaseHeader from '../components/User/BaseHeader'
-import { axiosInstance } from '../config/axiosInstance'
-import { useSelector, useDispatch } from 'react-redux'
-import { saveUser } from '../redux/features/userSlice'
+// import React, { useEffect, useState } from 'react'
+// import Header from '../components/User/Header'
+// import Footer from '../components/User/Footer'
+// import { Outlet, useLocation } from 'react-router-dom'
+// import BaseHeader from '../components/User/BaseHeader'
+// import { axiosInstance } from '../config/axiosInstance'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { saveUser } from '../redux/features/userSlice'
+
+// function UserLayout() {
+
+//   const { isUserAuth, userData } = useSelector((state) => state.user)
+//   const [isLoading,setIsLoading] = useState(true)
+//   const dispatch = useDispatch()
+//   const location = useLocation()
+
+//   const checkUser = () => {
+//     axiosInstance.get("/api/user/check")
+//       .then(res => {
+//         dispatch(saveUser(res.data))
+//         setIsLoading(false)
+
+//       })
+//       .catch(error => {
+//         console.log(error)
+//         setIsLoading(false)
+//       })
+//   }
+//   console.log("User Authentication =====",isUserAuth)
+
+//   useEffect(() => {
+//     checkUser()
+//   }, [location.pathname])
+
+//   return isLoading ? null : (
+//     <>
+    
+//       {/* Header */}
+//       {
+//         isUserAuth ? <Header /> : <BaseHeader />
+//       }
+
+
+//       {/* Page Details */}
+//       <div className='min-h-96 pt-10'>
+//         <Outlet />
+//       </div>
+
+//       {/* Footer */}
+//       <Footer />
+//     </>
+//   )
+// }
+
+// export default UserLayout
+
+import React, { useEffect, useState } from "react";
+import Header from "../components/User/Header";
+import Footer from "../components/User/Footer";
+import { Outlet, useLocation } from "react-router-dom";
+import BaseHeader from "../components/User/BaseHeader";
+import { axiosInstance } from "../config/axiosInstance";
+import { useSelector, useDispatch } from "react-redux";
+import { saveUser } from "../redux/features/userSlice";
 
 function UserLayout() {
-
-  const { isUserAuth, userData } = useSelector((state) => state.user)
-  const [isLoading,setIsLoading] = useState(true)
-  const dispatch = useDispatch()
-  const location = useLocation()
-
-  const checkUser = () => {
-    axiosInstance.get("/api/user/check")
-      .then(res => {
-        dispatch(saveUser(res.data))
-        setIsLoading(false)
-
-      })
-      .catch(error => {
-        console.log(error)
-        setIsLoading(false)
-      })
-  }
-  console.log("User Authentication =====",isUserAuth)
+  const { isUserAuth, userData } = useSelector((state) => state.user);
+  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
+  const location = useLocation();
 
   useEffect(() => {
-    checkUser()
-  }, [location.pathname])
+    if (!isUserAuth) {
+      // Only check user if not already authenticated
+      axiosInstance
+        .get("/api/user/check")
+        .then((res) => {
+          dispatch(saveUser(res.data));
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    } else {
+      setIsLoading(false);
+    }
+  }, []);
+
+  console.log("User Authentication =====", isUserAuth);
 
   return isLoading ? null : (
     <>
-    
       {/* Header */}
-      {
-        isUserAuth ? <Header /> : <BaseHeader />
-      }
-
+      {isUserAuth ? <Header /> : <BaseHeader />}
 
       {/* Page Details */}
-      <div className='min-h-96 pt-10'>
+      <div className="min-h-96 pt-10">
         <Outlet />
       </div>
 
       {/* Footer */}
       <Footer />
     </>
-  )
+  );
 }
 
-export default UserLayout
+export default UserLayout;
