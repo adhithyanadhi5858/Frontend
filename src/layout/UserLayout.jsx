f10769d17e7b3478d226d5c841ffb08150ba1997
Adithyan K Chandran <adhithyanadhi5858@gmail.com>
2025-06-28 12:30:17 +0530

    Project Finished

--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -1,59 +1,3 @@
-// import React, { useEffect, useState } from 'react'
-// import Header from '../components/User/Header'
-// import Footer from '../components/User/Footer'
-// import { Outlet, useLocation } from 'react-router-dom'
-// import BaseHeader from '../components/User/BaseHeader'
-// import { axiosInstance } from '../config/axiosInstance'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { saveUser } from '../redux/features/userSlice'
-
-// function UserLayout() {
-
-//   const { isUserAuth, userData } = useSelector((state) => state.user)
-//   const [isLoading,setIsLoading] = useState(true)
-//   const dispatch = useDispatch()
-//   const location = useLocation()
-
-//   const checkUser = () => {
-//     axiosInstance.get("/api/user/check")
-//       .then(res => {
-//         dispatch(saveUser(res.data))
-//         setIsLoading(false)
-
-//       })
-//       .catch(error => {
-//         console.log(error)
-//         setIsLoading(false)
-//       })
-//   }
-//   console.log("User Authentication =====",isUserAuth)
-
-//   useEffect(() => {
-//     checkUser()
-//   }, [location.pathname])
-
-//   return isLoading ? null : (
-//     <>
-    
-//       {/* Header */}
-//       {
-//         isUserAuth ? <Header /> : <BaseHeader />
-//       }
-
-
-//       {/* Page Details */}
-//       <div className='min-h-96 pt-10'>
-//         <Outlet />
-//       </div>
-
-//       {/* Footer */}
-//       <Footer />
-//     </>
-//   )
-// }
-
-// export default UserLayout
-
 import React, { useEffect, useState } from "react";
 import Header from "../components/User/Header";
 import Footer from "../components/User/Footer";
@@ -72,8 +16,7 @@ function UserLayout() {
   useEffect(() => {
     if (!isUserAuth) {
       // Only check user if not already authenticated
-      axiosInstance
-        .get("/api/user/check")
+      axiosInstance.get("/api/user/check")
         .then((res) => {
           dispatch(saveUser(res.data));
         })
@@ -108,59 +51,4 @@ function UserLayout() {
 
 export default UserLayout;
 
-
-// import React, { useEffect, useState } from "react";
-// import Header from "../components/User/Header";
-// import Footer from "../components/User/Footer";
-// import { Outlet, useLocation } from "react-router-dom";
-// import BaseHeader from "../components/User/BaseHeader";
-// import { axiosInstance } from "../config/axiosInstance";
-// import { useSelector, useDispatch } from "react-redux";
-// import { saveUser, clearUser } from "../redux/features/userSlice";
-
-// function UserLayout() {
-//   const { isUserAuth } = useSelector((state) => state.user);
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [justLoggedOut, setJustLoggedOut] = useState(false); // Prevents re-fetching after logout
-//   const dispatch = useDispatch();
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     if (justLoggedOut) {
-//       setJustLoggedOut(false); // Reset flag after handling logout
-//       setIsLoading(false);
-//       return;
-//     }
-
-//     axiosInstance
-//       .get("/api/user/check")
-//       .then((res) => {
-//         dispatch(saveUser(res.data)); // Update Redux state if user is authenticated
-//       })
-//       .catch((err) => {
-//         console.log("User is not authenticated");
-//         dispatch(clearUser()); // Ensure user state is false
-//       })
-//       .finally(() => {
-//         setIsLoading(false);
-//       });
-//   }, [location.pathname]);
-
-//   return isLoading ? null : (
-//     <>
-//       {/* Header */}
-//       {isUserAuth ? <Header /> : <BaseHeader />}
-
-//       {/* Page Details */}
-//       <div className="min-h-96 pt-10">
-//         <Outlet />
-//       </div>
-
-//       {/* Footer */}
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default UserLayout;
 
